58732dbdd4e95806fd3df8140eb28ab1fde21799
Marquise-williams35 <mwilliams482@boyslatin.net>
2026-04-09 13:38:10 +0000

    API work

--- PlaceHolderApi.py
+++ PlaceHolderApi.py
@@ -1,6 +1,11 @@
 import requests
 
-url= ''
+url= 'https://bored-api.appbrewery.com/random'
 response = requests.get(url)
+
+print(response.json())
+
+url2= 'https://pokeapi.co/api/v2/pokemon/pikachu'
+response = requests.get(url2)
 
 print(response.json())
