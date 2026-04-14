852d2912d57b6be7e315ad7b68755b0ff02c593c
Marquise-williams35 <mwilliams482@boyslatin.net>
2026-04-14 13:33:14 +0000

    Pokemon API  classwork

--- PlaceHolderApi.py
+++ PlaceHolderApi.py
@@ -1,11 +1,42 @@
 import requests
 
-url= 'https://bored-api.appbrewery.com/random'
-response = requests.get(url)
+query = 'https://pokeapi.co/api/v2/pokemon/rattata/'
+response = requests.get(query)
 
-print(response.json())
 
-url2= 'https://pokeapi.co/api/v2/pokemon/pikachu'
-response = requests.get(url2)
+if response.status_code == 200:
+    data = response.json()
+    
+    filtered_data = {
+        "name": data["name"],
+        "height":data["height"],
+        "weight":data["weight"],
+        "types":data["types"]
+     }
+    print(filtered_data)
+else:
+    print('data not found')    
+    print(response.status_code)
+    
 
-print(response.json())
+
+
+
+
+#url= 'https://bored-api.appbrewery.com/random'
+#response = requests.get(url)
+
+#print(response.json())
+
+#url2= 'https://pokeapi.co/api/v2/pokemon/Rattata '
+#response2 = requests.get(url2)
+
+#print(response2.json())
+
+
+
+
+
+
+
+
