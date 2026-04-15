ea9fe0a6139c72ca174b555fa563c06c26009dcd
Marquise-williams35 <mwilliams482@boyslatin.net>
2026-04-15 13:34:37 +0000

    student api

---
+++ StudentApi.py
@@ -0,0 +1,19 @@
+import requests
+
+rick_morty='https://rickandmortyapi.com/api/character/1,183,2'
+response = requests.get(rick_morty)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+
+    #filtered_data = {
+    #    "name": data["name"],
+    #    "status":data["status"],
+    #    "species":data["species"],
+    #    "gender":data["gender"]
+    # }
+    #print(filtered_data)
+else:
+    print('data not found')    
+    print(response.status_code)
