ceb483357a404a501a4a13153ed52134b57b332f
Marquise-williams35 <mwilliams482@boyslatin.net>
2026-04-29 13:37:22 +0000

    SQL WORK

--- sql_intro.py
+++ sql_intro.py
@@ -2,20 +2,26 @@
 import sqlite3
 
 #2. the connect method creates?starts our database
-connect = sqlite3.connect()
+connect = sqlite3.connect('test.db')
 
 #3. the cursor variable creates a new object that lets us send objects to our database
 cursor = connect.cursor()
 
 #4. we need to create a schema (structure) for our data
-cursor.execute('''(
-    CREATE TABLE compers(
-    id INTERGER PRIMARY computers,
+cursor.execute('''
+    CREATE TABLE computers(
+    id INTERGER PRIMARY KEY,
     model TEXT,
     color TEXT,
-    hasWebCam BOOL, 
+    hasWebCam BOOL,
+    memory INTERGER,
+    price INTERGER
                )''')
 
 cursor.execute('''
-    INJSERT INTO COMPUTERS(model,color,hasWebcam,memory,price)
-    values('apple m4'')
+    INSERT INTO computers(model,color,hasWebcam,memory,price)
+    values('apple m4','blue',True, 8, 1500)''')
+
+connect.commit()
+
+connect.close()
